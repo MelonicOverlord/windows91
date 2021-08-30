14adcd443cc42f7adae25f1eea201899a7ab963a
Stilic <lateman-jpeg@outlook.fr>
2021-08-30 15:01:36 +0200

    fix api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,62 @@
 import $ from "jquery";
+import "webpack-jquery-ui/css";
+import "webpack-jquery-ui/draggable";
+import "webpack-jquery-ui/resizable";
 
-export default new class window {
-    create(name) {
-        $("body").append("<p>" + name + "</p>");
+import "98.css";
+import "animate.css";
+import "./style.scss";
+
+class window {
+    update(data={"maximizable": true}) {
+        $(".window")
+            .resizable({ minWidth: 300, minHeight: 100 })
+            .draggable({ handle: ".title-bar" });
+        let maximizeWindow = function (obj) {
+            if (obj.parent().hasClass("maximized")) {
+                obj.parent().removeClass("maximized");
+            } else {
+                obj.parent().addClass("maximized");
+            }
+        };
+        $(".window .title-bar").on("dblclick", function () {
+            if (data["maximizable"] == true) {
+                maximizeWindow($(this));
+            }
+        });
+        $(".window .title-bar .maximize-window").on("click", function () {
+            maximizeWindow($(".window .title-bar"));
+        });
+        $(".window .close-window").on("click", function () {
+            $(".window").removeClass("animate__rubberBand");
+            $(".window").addClass("animate__bounceOut");
+            $(".window").on("animationend", function () {
+                $(".window").remove();
+            });
+        });
+    }
+    create(title, content, data = { "maximizable": true }) {
+        $("body").append(`
+        <div class="window animate__animated animate__rubberBand" style="width: 300px; height: 100px;">
+            <div class="title-bar">
+                <div class="title-bar-text">${title}</div>
+                <div class="title-bar-controls">
+                    <button aria-label="Minimize"></button>
+                    ${
+                        data["maximizable"]
+                            ? '<button class="maximize-window" aria-label="Maximize"></button>'
+                            : ""
+                    }
+                    <button class="close-window" aria-label="Close"></button>
+                </div>
+            </div>
+            <div class="window-body">
+                ${content}
+            </div>
+        </div>
+        `);
+        this.update({ maximized: data["maximizable"] });
     }
 }
+
+export let wnd = new window();
--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
-import $ from "jquery";
-import "webpack-jquery-ui/draggable";
-import "webpack-jquery-ui/resizable";
-import "webpack-jquery-ui/css";
+import { wnd } from "./api";
 
-import "./style.scss";
-import "98.css";
-import "animate.css";
-import api from "./api";
-api.create("test");
-
-$(".window")
-    .draggable({ handle: ".title-bar" })
-    .resizable({ minWidth: 300, minHeight: 100 });
-function maximizeWindow(obj) {
-    if (obj.parent().hasClass("maximized")) {
-        obj.parent().removeClass("maximized");
-    } else {
-        obj.parent().addClass("maximized");
-    }
-}
-$(".window .title-bar").on("dblclick", function () {
-    maximizeWindow($(this));
-});
-$(".window .title-bar .max").on("click", function () {
-    maximizeWindow($(this).parent().parent());
-});
-
-$(".window .title-bar .close").on("click", function () {
-    $(this).parent().parent().parent().remove();
-});
+wnd.create("Welcome", `
+    <p>Welcome to Windows 91, the only one really open-source WebOS of the world!</p>
+    <button class="close-window">OK</button>
+`, {"maximizable": false});
