42f91facb080948b57a4af86b335c51fbb16624d
Stilic <lateman-jpeg@outlook.fr>
2021-08-30 09:02:35 +0200

    fix rlly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-// import $ from "jquery";
-// import 'jquery-ui';
+import $ from "jquery";
+import "webpack-jquery-ui/draggable";
+import "webpack-jquery-ui/resizable";
+import "webpack-jquery-ui/css";
+window.jQuery = $;
+window.$ = $;
 import "./style.scss";
+import "98.css";
 
-console.log("Hello from webpack");
+$(".window")
+    .draggable({ handle: ".title-bar" })
+    .resizable({ minWidth: 300, minHeight: 100 });
+function maximizeWindow(obj) {
+    console.log(obj);
+    if (obj.parent().hasClass("maximized")) {
+        obj.parent().removeClass("maximized");
+    } else {
+        obj.parent().addClass("maximized");
+    }
+}
+$(".window .title-bar").on("dblclick", function(){
+    maximizeWindow($(this));
+});
+$(".window .title-bar .max").on("click", function(){
+    maximizeWindow($(this).parent().parent());
+})
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
+    resolve: {
+        alias: {
+            'jquery': 'jquery/src/jquery'
+        }
+      },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
@@ -16,17 +21,28 @@ module.exports = {
         compress: true,
     },
     output: {
-        filename: "bundle.js",
+        filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
     module: {
         rules: [
             {
-                test: /\.scss$/i,
-                exclude: /node_modules/,
+                test: /\.(sass|css|scss)$/,
                 use: ["style-loader", "css-loader", "sass-loader"],
             },
         ],
+    },
+    optimization: {
+        runtimeChunk: "single",
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendors",
+                    chunks: "all",
+                },
+            },
+        },
     },
 };
