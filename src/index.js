96521725c436acaa0d714e6949860e4e83980c32
Stilic <lateman-jpeg@outlook.fr>
2021-08-30 15:02:14 +0200

    better welcome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { wnd } from "./api";
 
 wnd.create("Welcome", `
-    <p>Welcome to Windows 91, the only one really open-source WebOS of the world!</p>
+    <p>Welcome to Windows 91, a open-source WebOS build for funny utility!</p>
     <button class="close-window">OK</button>
 `, {"maximizable": false});
